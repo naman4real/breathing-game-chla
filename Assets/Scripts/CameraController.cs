39aaabd5887681e11772bd7c14c1e457ebb12299
omrifried <omrifried@gmail.com>
2020-02-26 11:18:39 -0800

    Script Updates

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,10 +6,6 @@ public class CameraController : MonoBehaviour
 {
     public GameObject player;
     private Vector3 offset = new Vector3(13.69f, 19.96f, -0.32f);
-    //private float horizontalInput;
-    //private float turnSpeed = 25f;
-    //private float windowLimit = 10f;
-    // Start is called before the first frame update
     void Start()
     {
 
@@ -19,12 +15,6 @@ public class CameraController : MonoBehaviour
     void Update()
     {
         transform.position = player.transform.position + offset;
-        //horizontalInput += Input.GetAxis("Horizontal") * Time.deltaTime * turnSpeed;
-        //horizontalInput = Mathf.Clamp(horizontalInput, -windowLimit, windowLimit);
-        //transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, 270 + horizontalInput, transform.localEulerAngles.z);
-        // Offset camera behind ship.
-
-        //transform.Rotate(new Vector3(0, 1, 0), Time.deltaTime * turnSpeed * horizontalInput);
 
     }
 }
--- Assets/Scripts/MainBoatController.cs
+++ Assets/Scripts/MainBoatController.cs
@@ -6,27 +6,28 @@ public class MainBoatController : MonoBehaviour
 {
     private float speed = 20f;
     private float speedMultiplier = 2.5f;
+    private float appearTime = 1000f;
     private bool breathIsOn = false;
-    private float horizontalInput;
-    private float turnSpeed = 80f;
     private bool collision = false;
+    public Renderer gameBoat;
+    public float blinkInterval = 1f;
+    public float blinkStartDelay = 0.0f;
     private Rigidbody playerBody;
     // Start is called before the first frame update
     void Start()
     {
         playerBody = GetComponent<Rigidbody>();
+        gameBoat = GetComponent<Renderer>();
+        gameBoat.enabled = true;
     }
 
     // Update is called once per frame.
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
-        // If arrow is down, turn on accelerator.
+        // If arrow is down, turn on breath.
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             breathIsOn = true;
-            //playerBody.AddForce(-transform.right * speed * speedMultiplier, ForceMode.Impulse);
         }
         if (Input.GetKeyUp(KeyCode.UpArrow))
         {
@@ -38,12 +39,12 @@ public class MainBoatController : MonoBehaviour
     private void FixedUpdate()
     {
         // Move the boat forward at constant pace.
-        if (!breathIsOn && !collision)
+        if (!breathIsOn)
         {
-            //transform.Translate(Vector3.left * Time.deltaTime * speed);
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
         }
         // Accelerate boat.
-        if (breathIsOn && !collision)
+        if (breathIsOn)
         {
             transform.Translate(Vector3.left * Time.deltaTime * speed * speedMultiplier);
         }
@@ -51,8 +52,27 @@ public class MainBoatController : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        playerBody.useGravity = true;
+        Destroy(other.gameObject);
         collision = true;
+        // If the boat collides with an object, blink on and off. 
+        StartCoroutine(BlinkTime(2.5f));
     }
 
+    // Blink the boat on and off.
+    private IEnumerator BlinkTime(float blinkDuration)
+    {
+        float timeCounter = 0;
+        while (timeCounter < blinkDuration)
+        {
+            // make the boat blink off and on.
+            gameBoat.enabled = !gameBoat.enabled;
+            //wait 1 second per interval
+            yield return new WaitForSeconds(0.3f);
+            timeCounter += (1f/3f);
+        }
+        gameBoat.enabled = true;
+        collision = false;
+    }
 }
+
+
